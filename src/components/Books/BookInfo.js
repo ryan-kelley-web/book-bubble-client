478be49237b91df67ec17e0f1a97e4e7b74065a9
Sean Dwyer <spdwyer06@gmail.com>
2020-11-21 12:45:05 -0500

    Full functionality between book info and book edit on all shelf components

--- src/components/Books/BookInfo.js
+++ src/components/Books/BookInfo.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Form, FormGroup, Label, Container, Row, Col, Progress} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Container, Row, Col, Progress, Modal, ModalBody} from 'reactstrap';
 
 
 const BookInfo = (props) => {
@@ -15,15 +15,69 @@ const BookInfo = (props) => {
             })
         };
         fetch(url, options)
-            .then(() => props.fetchBooks())
+            .then(() => {
+                props.fetchBooks();
+                props.infoOff();
+            })
             .catch(err => console.log('Book Info Fetch Error:', err));
     };
 
-    
-
 
     return(
-        <Container>
+        <Modal isOpen={true}>
+            <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Button onClick={props.infoOff}>X</Button>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='author'>Author:</Label>
+                        <Label name='author'>{props.book.author}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='title'>Title:</Label>
+                        <Label name='title'>{props.book.title}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='genre'>Genre:</Label>
+                        <Label name='genre'>{props.book.genre}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='numOfPages'>Total Number Of Pages:</Label>
+                        <Label name='numOfPages'>{props.book.total_pages}</Label>
+                        {/* //attempting progress bar add */}
+                        {/* <Progress value="25">25%</Progress> */}
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='yearPublished'>Year Published:</Label>
+                        <Label name='yearPublished'>{props.book.year_published}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='description'>Book Description:</Label>
+                        <Label name='description'>{props.book.description}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='readStatus'>Book Status:</Label>
+                        <Label name='readStatus'>{props.book.read_status}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='rating'>Rating:</Label>
+                        <Label name='rating'>{props.book.rating}</Label>
+                    </FormGroup>
+                    <Button color='warning' onClick={props.updateOn}>Edit Book</Button>
+                    <Button color='danger' onClick={() => deleteBook(props.book)}>Delete Book</Button>
+                </Form>
+            </ModalBody>
+        </Modal>
+    );
+};
+
+export default BookInfo;
+
+
+/*
+
+<Container>
             <Form>
                 <FormGroup>
                     <Label htmlFor='author'>Author:</Label>
@@ -40,30 +94,26 @@ const BookInfo = (props) => {
                 <FormGroup>
                     <Label htmlFor='numOfPages'>Total Number Of Pages:</Label>
                     <Label name='numOfPages'>{props.book.total_pages}</Label>
-                    {/* //attempting progress bar add */}
-                    {/* <Progress value="25">25%</Progress> */}
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='yearPublished'>Year Published:</Label>
-                    <Label name='yearPublished'>{props.book.year_published}</Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='description'>Book Description:</Label>
-                    <Label name='description'>{props.book.description}</Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='readStatus'>Book Status:</Label>
-                    <Label name='readStatus'>{props.book.read_status}</Label>
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor='rating'>Rating:</Label>
-                    <Label name='rating'>{props.book.rating}</Label>
-                </FormGroup>
-                <Button color='warning' onClick={props.BookEdit}>Edit Book</Button>
-                <Button color='danger' onClick={() => deleteBook(props.book)}>Delete Book</Button>
-            </Form>
-        </Container>
-    );
-};
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='yearPublished'>Year Published:</Label>
+                        <Label name='yearPublished'>{props.book.year_published}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='description'>Book Description:</Label>
+                        <Label name='description'>{props.book.description}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='readStatus'>Book Status:</Label>
+                        <Label name='readStatus'>{props.book.read_status}</Label>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label htmlFor='rating'>Rating:</Label>
+                        <Label name='rating'>{props.book.rating}</Label>
+                    </FormGroup>
+                    <Button color='warning' onClick={props.updateOn}>Edit Book</Button>
+                    <Button color='danger' onClick={() => deleteBook(props.book)}>Delete Book</Button>
+                </Form>
+            </Container>
 
-export default BookInfo;
+*/
